9179d2c1aa85286f0fbbf23d5c1a9c79d8a3c406
SEO-J17 <qw4643987@naver.com>
2022-11-15 03:36:19 +0900

    #4 remove: 필요없는 파일 삭제

--- app/src/main/java/io/github/seoj17/footballscore/data/AwayTeam.kt
+++
@@ -1,9 +0,0 @@
-package io.github.seoj17.footballscore.data
-
-data class AwayTeam(
-    val crest: String,
-    val id: Int,
-    val name: String,
-    val shortName: String,
-    val tla: String
-)
--- app/src/main/java/io/github/seoj17/footballscore/data/HalfTime.kt
+++
@@ -1,6 +0,0 @@
-package io.github.seoj17.footballscore.data
-
-data class HalfTime(
-    val away: Int?,
-    val home: Int?
-)
