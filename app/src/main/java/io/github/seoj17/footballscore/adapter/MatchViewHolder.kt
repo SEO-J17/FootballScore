29567fec134b586a556365b51b9b8e8dd1ccb762
SEO-J17 <qw4643987@naver.com>
2022-11-17 00:27:48 +0900

    #8 :hammer: root에서 context로 수정함

--- app/src/main/java/io/github/seoj17/footballscore/adapter/MatchViewHolder.kt
+++ app/src/main/java/io/github/seoj17/footballscore/adapter/MatchViewHolder.kt
@@ -6,40 +6,28 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import io.github.seoj17.footballscore.data.Matches
 import io.github.seoj17.footballscore.databinding.ViewHolderMatchBinding
+import io.github.seoj17.footballscore.utils.StatusSet
 
 class MatchViewHolder(
     private val binding: ViewHolderMatchBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(matches: Matches) {
         with(binding) {
-            Glide.with(root).load(matches.homeTeam.crest).into(homeTeam)
+            Glide.with(root.context).load(matches.homeTeam.crest).into(homeTeam)
             homeName.text = matches.homeTeam.shortName
-            homeScore.text = matches.score.fullTime.home?.toString() ?: DEFAULT_SCORE
+            homeScore.text = matches.score.fullTime.home?.toString() ?: StatusSet.DEFAULT_SCORE
 
-            gameStatus.text = matches.status.setStatus()
+            gameStatus.text = StatusSet.getStatus(matches.status)
             leagueName.text = matches.competition.code
 
-            Glide.with(root).load(matches.awayTeam.crest).into(awayTeam)
+            Glide.with(root.context).load(matches.awayTeam.crest).into(awayTeam)
             awayName.text = matches.awayTeam.shortName
-            awayScore.text = matches.score.fullTime.away?.toString() ?: DEFAULT_SCORE
+            awayScore.text = matches.score.fullTime.away?.toString() ?: StatusSet.DEFAULT_SCORE
 
-        }
-    }
-
-    private fun String.setStatus(): String {
-        return if (this == GAME_STATE) {
-            GAME_PROGRESS
-        } else {
-            GAME_FINISH
         }
     }
 
     companion object {
-        private const val GAME_STATE = "TIMED"
-        private const val GAME_PROGRESS = "진행중"
-        private const val GAME_FINISH = "종료"
-        private const val DEFAULT_SCORE = "-"
-
         operator fun invoke(parent: ViewGroup): MatchViewHolder {
             return MatchViewHolder(
                 ViewHolderMatchBinding.inflate(
