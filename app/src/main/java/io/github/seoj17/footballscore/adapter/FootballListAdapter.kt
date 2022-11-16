7bf24811e17a5f3798ca1fe3de766e6046289b3e
SEO-J17 <qw4643987@naver.com>
2022-11-16 22:43:15 +0900

    #8 :hammer: 실제 데이터 타입 적용함

--- app/src/main/java/io/github/seoj17/footballscore/adapter/FootballListAdapter.kt
+++ app/src/main/java/io/github/seoj17/footballscore/adapter/FootballListAdapter.kt
@@ -2,14 +2,15 @@ package io.github.seoj17.footballscore.adapter
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.github.seoj17.footballscore.data.Matches
 
 class FootballListAdapter(
-    private val dataSet: List<String> = listOf(),
+    private val dataSet: List<Matches> = listOf(),
 ) : RecyclerView.Adapter<MatchViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MatchViewHolder(parent)
 
     override fun onBindViewHolder(holder: MatchViewHolder, position: Int) {
-        with(holder){
+        with(holder) {
             bind(dataSet[position])
         }
     }
