d29432b04f2cb4d70c3fcd384861aed9b942fe40
SEO-J17 <qw4643987@naver.com>
2022-11-16 22:41:35 +0900

    #8 :hammer: 코루틴으로 데이터 받아오는 코드 구현

--- app/src/main/java/io/github/seoj17/footballscore/MainActivity.kt
+++ app/src/main/java/io/github/seoj17/footballscore/MainActivity.kt
@@ -2,10 +2,29 @@ package io.github.seoj17.footballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import io.github.seoj17.footballscore.adapter.FootballListAdapter
+import io.github.seoj17.footballscore.databinding.ActivityMainBinding
+import io.github.seoj17.footballscore.network.FootballService
+import io.github.seoj17.footballscore.utils.QueryUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        lifecycleScope.launch {
+            with(FootballService.getMatchesInfo()) {
+                binding.recyclerView.adapter = FootballListAdapter(QueryUtils.extractData(this))
+            }
+            withContext(Dispatchers.Main) {
+                binding.loadingBar.visibility = View.GONE
+            }
+        }
     }
 }
