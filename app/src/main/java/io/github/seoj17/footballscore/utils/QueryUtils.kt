b59e34439343d765d689a8eeb403560f0afe1acb
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2022-11-25 17:00:26 +0900

    #14 :sparkles: 받아온 데이터 사용할 수 있게 구현함 (#37)

--- app/src/main/java/io/github/seoj17/footballscore/utils/QueryUtils.kt
+++ app/src/main/java/io/github/seoj17/footballscore/utils/QueryUtils.kt
@@ -2,14 +2,25 @@ package io.github.seoj17.footballscore.utils
 
 import io.github.seoj17.footballscore.data.FootballInfo
 import io.github.seoj17.footballscore.data.Matches
+import io.github.seoj17.footballscore.data.Squad
+import io.github.seoj17.footballscore.data.TeamDetail
 
 object QueryUtils {
     private val dataSet = mutableListOf<Matches>()
+    private val teamDetailData = mutableListOf<Squad>()
     fun extractData(matchInfo: FootballInfo?): List<Matches> {
         return (matchInfo?.matches?.map { matches ->
             Matches(matches)
         } ?: listOf()).also {
             dataSet.addAll(it)
+        }
+    }
+
+    fun extractDetailTeam(teamDetail: TeamDetail?): List<Squad> {
+        return (teamDetail?.squad?.map { player ->
+            Squad(player)
+        } ?: listOf()).also {
+            teamDetailData.addAll(it)
         }
     }
 }
