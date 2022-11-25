0b85d7daab4c8a42e15b0ff43bb56dbf66c95c5a
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2022-11-25 16:03:06 +0900

    #12 팀 API 요청 인터페이스 구현 (#35)
    * #12 :sparkles: 팀에 대한 API 요청 코드 작성함
    
    * #12 :hammer: 잘못된 타입 Int형으로 수정함

--- app/src/main/java/io/github/seoj17/footballscore/network/FootballAPI.kt
+++ app/src/main/java/io/github/seoj17/footballscore/network/FootballAPI.kt
@@ -1,10 +1,15 @@
 package io.github.seoj17.footballscore.network
 
 import io.github.seoj17.footballscore.data.FootballInfo
+import io.github.seoj17.footballscore.data.TeamDetail
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface FootballAPI {
     @GET("/v4/matches")
     suspend fun getMatches(): FootballInfo
+
+    @GET("/v4/teams/{id}")
+    suspend fun getTeamInfo(@Path("id") id: Int): TeamDetail
 }
