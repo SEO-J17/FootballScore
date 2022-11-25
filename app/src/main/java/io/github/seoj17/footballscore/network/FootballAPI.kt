cf515792ff311176c6fa2f448dae1d352eae0a11
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2022-11-25 15:58:54 +0900

    #24 :hammer: auth인터셉터 구현으로 인증 방식 변경함 (#26)
    * #24 :hammer: auth인터셉터 구현으로 인증 방식 변경함
    
    * #24 :hammer: http인터셉터 수정함

--- app/src/main/java/io/github/seoj17/footballscore/network/FootballAPI.kt
+++ app/src/main/java/io/github/seoj17/footballscore/network/FootballAPI.kt
@@ -6,7 +6,5 @@ import retrofit2.http.Header
 
 interface FootballAPI {
     @GET("/v4/matches")
-    suspend fun getMatches(
-        @Header("X-Auth-Token") authToken: String,
-    ): FootballInfo
+    suspend fun getMatches(): FootballInfo
 }
--- app/src/main/java/io/github/seoj17/footballscore/network/FootballService.kt
+++ app/src/main/java/io/github/seoj17/footballscore/network/FootballService.kt
@@ -1,5 +1,7 @@
 package io.github.seoj17.footballscore.network
 
+import io.github.seoj17.footballscore.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,11 +12,27 @@ object FootballService {
     private const val REQUEST_URL = "https://api.football-data.org"
     private const val AUTH_TOKEN = "ca3c4e5c4ffd4022baa96466f948d4ae"
     private const val TIME_OUT = 10000L
-    private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    private val httpInterceptor = HttpLoggingInterceptor().apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    private val authInterceptor = Interceptor { chain ->
+        chain.proceed(
+            chain.request()
+                .newBuilder()
+                .addHeader("X-Auth-Token", AUTH_TOKEN)
+                .build()
+        )
+    }
 
     private val client =
         OkHttpClient.Builder()
-            .addInterceptor(interceptor)
+            .addInterceptor(httpInterceptor)
+            .addInterceptor(authInterceptor)
             .connectTimeout(TIME_OUT, TimeUnit.MILLISECONDS)
             .build()
 
@@ -27,6 +45,6 @@ object FootballService {
 
     private val api = requestObject.create(FootballAPI::class.java)
 
-    suspend fun getMatchesInfo() = api.getMatches(AUTH_TOKEN)
+    suspend fun getMatchesInfo() = api.getMatches()
 
 }
