f681c2b944ee229defbc5b3a675e064a8265ddb5
SEO-J17 <qw4643987@naver.com>
2022-11-25 17:07:36 +0900

    #29 :hammer: XML파일와 액티비티 이름 통일 시킴

--- app/src/main/java/io/github/seoj17/footballscore/TeamDetailActivity.kt
+++ app/src/main/java/io/github/seoj17/footballscore/TeamDetailActivity.kt
@@ -3,13 +3,13 @@ package io.github.seoj17.footballscore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import io.github.seoj17.footballscore.databinding.ActivityDetailBinding
+import io.github.seoj17.footballscore.databinding.TeamDetailActivityBinding
 
 class TeamDetailActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityDetailBinding
+    private lateinit var binding: TeamDetailActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
+        binding = TeamDetailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
